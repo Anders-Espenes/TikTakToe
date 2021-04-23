f9f4c5a5d68a72e1306a6bfdd2dcc50f07417f8b
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-04-24 01:20:02 +0200

    Adde joinGame post request, and some tests

--- app/src/main/java/com/example/tiktaktoe/Api/GameService.kt
+++ app/src/main/java/com/example/tiktaktoe/Api/GameService.kt
@@ -1,6 +1,6 @@
 package com.example.tiktaktoe.Api
 
-import com.android.volley.Request
+import android.util.Log
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
@@ -34,6 +34,16 @@ object GameService {
                 context.getString(R.string.domain),
                 context.getString(R.string.base_path)
             )
+        ),
+
+        // https://generic-game-service.herokuapp.com/Game/<gameId>/join
+        JOIN_GAME(
+            "%1s%2s%3s%4s".format(
+                context.getString(R.string.protocol),
+                context.getString(R.string.domain),
+                context.getString(R.string.base_path),
+                context.getString(R.string.join_game_path)
+            )
         )
     }
 
@@ -43,22 +53,53 @@ object GameService {
         // Get url to API
         val url = APIEndpoints.CREATE_GAME.url
 
-        // Add player name, and gamestate
+        // Add player name and gamestate to request
         val requestData = JSONObject()
         requestData.put("player", playerId)
         requestData.putOpt("state", JSONArray(state))
 
         // Request Post, Url, Data, Headers
-        val request = object : JsonObjectRequest(Method.POST, url, requestData,
+        val request = object : JsonObjectRequest(
+            Method.POST, url, requestData,
             {
                 // Sucess game created
                 val game = Gson().fromJson(it.toString(), Game::class.java)
+                Log.d(TAG, game.toString())
                 callBack(game, null)
             }, {
                 // Error creating new game
                 callBack(null, it.networkResponse.statusCode)
             }) {
-            // Create headers for the request
+            // Add headers for the request
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String, String>()
+                headers["Content-type"] = "application/json"
+                headers["Game-Service-Key"] = context.getString(R.string.game_service_key)
+                return headers
+            }
+        }
+        requestQue.add(request)
+    }
+
+    fun joinGame(playerId: String, gameId: String, callback: GameServiceCallback) {
+        // Add gameId to URl
+        val url = APIEndpoints.JOIN_GAME.url.format(gameId)
+        Log.d(TAG, "URL: $url")
+
+        // Add playername and gameId to request
+        val requestData = JSONObject()
+        requestData.put("player", playerId)
+        requestData.put("gameId", gameId)
+
+        val request = object : JsonObjectRequest(Method.POST, url, requestData,
+            {
+                // Success game joined
+                val game = Gson().fromJson(it.toString(), Game::class.java)
+                callback(game, null)
+            }, {
+                // Error joining a game
+                callback(null, it.networkResponse.statusCode)
+            }) {
             override fun getHeaders(): MutableMap<String, String> {
                 val headers = HashMap<String, String>()
                 headers["Content-type"] = "application/json"
