ea99ed1d338cb48d6ad5afa1939507f760fffbb3
Anders Lauvrak Espenes <andersespenes@hotmail.com>
2021-04-23 16:47:31 +0200

    Installed volley, created app class to serve context, added gameservice object for networking

--- app/src/main/java/com/example/tiktaktoe/MainActivity.kt
+++ app/src/main/java/com/example/tiktaktoe/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.tiktaktoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiktaktoe.Api.GameService
+import com.example.tiktaktoe.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG:String = "MainActivity"
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        GameService.sendRequest()
     }
 }
